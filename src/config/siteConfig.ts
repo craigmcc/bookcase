4351ce8889cf4767afeaa412023d6aec21145c69
Craig McClanahan <craigmcc@gmail.com>
2023-07-02 18:25:00 -0700

    Add global site configuration variables object.

---
+++ src/config/siteConfig.ts
@@ -0,0 +1,47 @@
+// siteConfig.ts
+
+/**
+ * Configuration variables for the entire site.
+ *
+ * @packageDocumentation
+ */
+
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    description: "Library Management Application based on NextJS",
+    links: {
+        github: "https://github.com/craigmcc/bookcase"
+    },
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Libraries",
+            href: "/libraries",
+        },
+        {
+            title: "Authors",
+            href: "/authors",
+        },
+        {
+            title: "Series",
+            href: "/series",
+        },
+        {
+            title: "Stories",
+            href: "/stories",
+        },
+        {
+            title: "Volumes",
+            href: "/volumes",
+        },
+        {
+            title: "Users",
+            href: "/users",
+        },
+    ],
+    name: "Bookcase",
+}
