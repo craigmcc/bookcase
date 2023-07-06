5dce66cca521337efd4e7b3a72822c2a7a26b0b2
Craig McClanahan <craigmcc@gmail.com>
2023-07-06 11:41:16 -0700

    Install dependencies and configuration for Jest tests.

--- src/app/libraries/page.tsx
+++ src/app/libraries/page.tsx
@@ -24,37 +24,6 @@ async function getData(): Promise<LibraryActions.LibraryPlus[]> {
     return await LibraryActions.all();
 }
 
-/**
- * TODO: Dummy getData() method, will eventually use LibraryActions.all().
- */
-/*
-async function getData(): Promise<Library[]> {
-    return [
-        {
-            id: 101,
-            active: true,
-            name: "First Library",
-            notes: "Notes about First Library",
-            scope: "first",
-        },
-        {
-            id: 102,
-            active: false,
-            name: "Second Library",
-            notes: null,
-            scope: "second",
-        },
-        {
-            id: 103,
-            active: true,
-            name: "Third Library",
-            notes: "Notes about Third Library",
-            scope: "third",
-        },
-    ];
-}
-*/
-
 export default async function LibrariesPage() {
     const data = await getData();
     return (
