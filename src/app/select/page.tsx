016ff56ccbcc0c992f10bc047c174d41e93868a8
Craig McClanahan <craigmcc@gmail.com>
2023-07-24 14:39:40 -0700

    Configure themed colors (from Tailwind Elements).

---
+++ src/app/select/page.tsx
@@ -0,0 +1,17 @@
+// app/select/page.tsx
+
+/**
+ * Allow the user to select which Library they wish to manage,
+ * or (for superuser) select an administrative function.
+ *
+ * @packageDocumentation
+ */
+
+// External Modules ----------------------------------------------------------
+
+// Internal Modules ----------------------------------------------------------
+
+// Public Objects ------------------------------------------------------------
+
+
+
--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -26,7 +26,7 @@ export function SiteHeader() {
     // NOTE: session?.user?.scope is available if the user is signed in
 //    console.log("SiteHeader session", session);
     return (
-        <header className="sticky top-0 z-40 w-full border-b bg-indigo-50">
+        <header className="sticky top-0 z-40 w-full border-b bg-primary-100">
             <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
                 <MainNav items={siteConfig.mainNav}/>
                 <div className="flex flex-1 items-center justify-end space-x-4">
