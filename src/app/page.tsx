7a26c74d781e03a5a5ca95e883cddf64a195e959
Craig McClanahan <craigmcc@gmail.com>
2023-07-24 12:40:07 -0700

    Clean up the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+      <main className="flex flex-col items-center mt-6">
+        <Image
+            alt="Bookcase"
+            height={500}
+            src="/images/bookcase.jpg"
+            width={500}
+        />
+        <div className="mt-6 text-2xl font-bold">
+          Welcome to the Bookcase App!
+        </div>
+      </main>
+/*
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -109,5 +121,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+*/
   )
 }
