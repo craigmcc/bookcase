df86e95aae6a8815e122a98f7a85e2e9b8f5e268
Craig McClanahan <craigmcc@gmail.com>
2023-08-02 19:37:45 -0700

    First crack at local stories, but causes Storybook to crash.

---
+++ src/components/my/Button.stories.tsx
@@ -0,0 +1,40 @@
+// components/my/Button.stories.tsx
+
+/**
+ * Storybook stories for the Button component.
+ *
+ * @packageDocumentation
+ */
+
+// External Modules ----------------------------------------------------------
+
+import type {Meta, StoryObj} from "@storybook/react";
+
+// Internal Modules ----------------------------------------------------------
+
+import {Button} from "./Button";
+
+// Configuration Objects -----------------------------------------------------
+
+const meta: Meta<typeof Button> = {
+    component: Button,
+    title: "MyUI/Button",
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// Story Objects -------------------------------------------------------------
+
+export const Default: Story = {
+    render: () => <Button>Default</Button>
+}
+
+export const Primary: Story = {
+    render: () => <Button variant="primary">Primary</Button>
+}
+
+export const Secondary: Story = {
+    render: () => <Button variant="secondary">Secondary</Button>
+}
+
