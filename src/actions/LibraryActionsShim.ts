92f727fa9a0f12c311f4ad98390b5954eeaf3eea
Craig McClanahan <craigmcc@gmail.com>
2023-08-10 14:24:23 -0700

    Shim out the rest of the exported functions from LibraryActions.

--- src/actions/LibraryActionsShim.ts
+++ src/actions/LibraryActionsShim.ts
@@ -3,13 +3,15 @@
 // actions/LibraryActionsShim.ts
 
 /**
- * Attempt to work around Jest's rejection of pages that have "use server"
- * at the top, by providing a shim that just delegates to the real functions.
+ * Attempt to work around Jest's rejection of modules that have "use server"
+ * at the top, by providing a shim that just delegates to those real functions.
  *
  * @packageDocumentation
  */
 
 // External Modules ----------------------------------------------------------
+
+import {Prisma} from "@prisma/client";
 
 // Internal Modules ----------------------------------------------------------
 
@@ -17,8 +19,6 @@ import * as LibraryActions from "./LibraryActions";
 import {
     LibraryAllOptions,
     LibraryFindOptions,
-    LibraryIncludeOptions,
-    LibraryMatchOptions,
     LibraryPlus,
 } from "@/types/models/Library";
 
@@ -28,3 +28,22 @@ export const all = async (options?: LibraryAllOptions): Promise<LibraryPlus[]> =
     return await LibraryActions.all(options);
 }
 
+export const exact = async (name: string, options?: LibraryFindOptions): Promise<LibraryPlus> => {
+    return await LibraryActions.exact(name, options);
+}
+
+export const find = async (libraryId: number, options?: LibraryFindOptions): Promise<LibraryPlus> => {
+    return await LibraryActions.find(libraryId, options);
+}
+
+export const insert = async (library: Prisma.LibraryCreateInput): Promise<LibraryPlus> => {
+    return await LibraryActions.insert(library);
+}
+
+export const remove = async (libraryId: number): Promise<LibraryPlus> => {
+    return await LibraryActions.remove(libraryId);
+}
+
+export const update = async (libraryId: number, library: Prisma.LibraryUpdateInput): Promise<LibraryPlus> => {
+    return await LibraryActions.update(libraryId, library);
+}
