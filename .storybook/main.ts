44145ff2862c19b1656346ac376811f1874d1fa3
Craig McClanahan <craigmcc@gmail.com>
2023-12-07 16:33:20 -0800

    Update how storybook handles GFM, make it recognize @/... paths.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,40 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import remarkGfm from "remark-gfm";
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          remarkPlugins: [remarkGfm],
+        },
+      },
+    },
   ],
+  docs: {
+    autodocs: "tag",
+  },
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+    return config;
   },
 };
 export default config;
